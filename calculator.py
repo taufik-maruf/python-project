7888fd38ebfbeed1f97e1345719303ab2c0862ce
Taufik Ma'ruf <taufik.maruf123@gmail.com>
2025-05-30 09:50:52 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,18 +5,18 @@ print('B. Pembagian')
 print('C. Penambahan')
 print('D. Pengurangan')
 
-pilihan = input('Pilihan Anda: ')
+pilihan = input('Pilihan Anda: ').upper()
 
 nilai1 = int(input('Masukan Nilai Pertama:'))
 nilai2 = int(input('Masukan Nilai Kedua:'))
 jawaban = 0
-if pilihan == 'A' or 'a':
+if pilihan == 'A':
     jawaban = nilai1*nilai2
-elif pilihan == 'B' or 'b':
+elif pilihan == 'B':
     jawaban = nilai1/nilai2
-elif pilihan == 'C' or 'c':
+elif pilihan == 'C':
     jawaban = nilai1+nilai2
-elif pilihan == 'D' or 'd':
+elif pilihan == 'D':
     jawaban = nilai1-nilai2
 
 print(f'Hasil = {jawaban}')
